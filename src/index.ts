e6514f33beea0f3915160ded96f7f748c0355a93
lukabelani <luka.belani@gmail.com>
2021-02-02 19:09:44 +0100

    refactor methods to arrow functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'new name', age: 0 });
+
+user.on('change', () => {
+  console.log('user was changed');
+});
+
+// console.log(user.get('name'));
+
+user.trigger('change');
